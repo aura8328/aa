1729c351589111230f130d9c90a2e25f1b04972b
kylim <aura8328@gmail.com>
2016-06-24 18:11:38 +0900

    dIrectory retrieving

--- html_tag_export/html_tag_export.py
+++ html_tag_export/html_tag_export.py
@@ -1,5 +1,17 @@
 import urllib
+import platform
+import os
+import sys
 import bs4
+
+PLATFORM = platform.system()
+if PLATFORM == 'Windows':
+    DIR_TOC = '\\'
+elif PLATFORM == 'Linux':
+    DIR_TOC = '/'
+else:
+    DIR_TOC = '/'
+
 
 
 html= """
@@ -16,30 +28,78 @@ and they lived at the bottom of a well.</p>
 
 <p class="story">...</p>
 """
-input = 'html.body_1.p.a_2'
+input = 'html.body_1'
+
+def trans_param(input):
+    sparam = []
+    for sub in input.split('.'):
+        try:
+            sparam.append((sub.split('_')[0], sub.split('_')[1]))
+        except IndexError as e:
+            sparam.append((sub.split('_')[0], '0'))
+    # print sparam
+    return sparam
 
 
-sparam = []
-for sub in input.split('.'):
-    try:
-        sparam.append((sub.split('_')[0], sub.split('_')[1]))
-    except IndexError as e:
-        sparam.append((sub.split('_')[0], '0'))
+def find_html_from_tag(input, bsObj):
+    sparam = trans_param(input)
+    tmp_css = ""
+    for k, v in sparam:
+        if tmp_css != "":
+            tmp_css = tmp_css + ' > '
+        tmp_css = tmp_css + str(k)
+        if v == '0':
+            continue
+        tmp_css = tmp_css + ':nth-of-type('+ str(v) + ')'
+        # print tmp_css
+
+    return bsObj.select(tmp_css)
+
+"""
+
+Input Args 1 - Target Directory Root Path
+
+"""
+def main(argv):
+    if len(argv) != 2:
+        print('argv err')
+        exit(-1)
 
 
-print sparam
+    input = argv[0]
+    root_dir = argv[1]
+    result_root_dir = root_dir + '_' + input
+
+    for root, dirs, files in os.walk(root_dir):
+        for filename in files:
+            f_path = os.path.join(root, filename)
+
+            ## Result Directory Path Gen and Directory Create
+            t_path = f_path.split(root_dir)[-1]
+            result_dir = result_root_dir + t_path
+            try:
+                os.makedirs(os.path.dirname(result_dir))
+            except Exception as e:
+                # print("makedirs : {}".format(e))
+                pass
+
+            if os.path.splitext(f_path)[1] not in ['.html', '.htm']:
+                # print str(f_path) + ': is not html file.'
+                continue
+            with open(f_path, 'r') as f:
+                html = f.read()
+
+            soup = bs4.BeautifulSoup(html, 'html.parser')
+            try:
+                result = find_html_from_tag(input, soup)[0]
+            except IndexError as e:
+                pass
+
+            with open(result_dir, 'w') as f:
+                f.write(result.prettify())
+
+            # print result.prettify()
 
 
-soup = bs4.BeautifulSoup(html, 'html.parser')
-
-tmp_css = ""
-for k, v in sparam:
-    if tmp_css != "":
-        tmp_css = tmp_css + ' > '
-    tmp_css = tmp_css + str(k)
-    if v == '0':
-        continue
-    tmp_css = tmp_css + ':nth-of-type('+ str(v) + ')'
-    print tmp_css
-
-print soup.select(tmp_css)
+if __name__ == "__main__":
+   main(sys.argv[1:])
